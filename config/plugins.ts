6f3e7d7eacdfb43cbbe9b39175fac97de601063f
Noï van Mondfrans <noivanmondfrans@gmail.com>
2024-06-08 21:52:42 +0200

    Added email settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+export default ({env}) => ({
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: 'smtp.eu.sparkpostmail.com',
+                port: 587,
+                auth: {
+                    user: 'SMTP_Injection',
+                    pass: env('SPARKPOST_API_KEY'),
+                },
+            },
+            settings: {
+                defaultFrom: 'no-reply@letters.transutrechtbeyond.nl',
+            },
+        }
+    }
+});
