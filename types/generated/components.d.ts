6693d7bbdd94a04bc40e9cf81cc9d76081de7ccb
Noï van Mondfrans <noivanmondfrans@gmail.com>
2024-12-05 08:06:28 +0100

    Latest major strapi update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,48 +1,48 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface GeneralLink extends Schema.Component {
-  collectionName: 'components_general_links';
-  info: {
-    displayName: 'Link';
-    icon: 'earth';
-  };
-  attributes: {
-    Text: Attribute.String;
-    Target: Attribute.String;
-  };
-}
-
-export interface FooterSection extends Schema.Component {
-  collectionName: 'components_footer_sections';
-  info: {
-    displayName: 'Section';
-    icon: 'information';
-    description: '';
-  };
-  attributes: {
-    Description: Attribute.Text;
-    Links: Attribute.Component<'general.link', true>;
-    Title: Attribute.String;
-  };
-}
-
-export interface ArticleBasicText extends Schema.Component {
+export interface ArticleBasicText extends Struct.ComponentSchema {
   collectionName: 'components_article_basic_texts';
   info: {
     displayName: 'BasicText';
     icon: 'bold';
   };
   attributes: {
-    Content: Attribute.Blocks;
+    Content: Schema.Attribute.Blocks;
   };
 }
 
-declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
-      'general.link': GeneralLink;
-      'footer.section': FooterSection;
+export interface FooterSection extends Struct.ComponentSchema {
+  collectionName: 'components_footer_sections';
+  info: {
+    description: '';
+    displayName: 'Section';
+    icon: 'information';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text;
+    Links: Schema.Attribute.Component<'general.link', true>;
+    Title: Schema.Attribute.String;
+  };
+}
+
+export interface GeneralLink extends Struct.ComponentSchema {
+  collectionName: 'components_general_links';
+  info: {
+    displayName: 'Link';
+    icon: 'earth';
+  };
+  attributes: {
+    Target: Schema.Attribute.String;
+    Text: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
       'article.basic-text': ArticleBasicText;
+      'footer.section': FooterSection;
+      'general.link': GeneralLink;
     }
   }
 }
